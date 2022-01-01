45f309af1106b925a40342a3a451370d6085e390
Owen S <owen.skilton@gmail.com>
2022-01-01 03:34:44 +0000

    Favourites remembered and rendered when searching again. Need to add remove favourite function and add all favourites to profile screen. :) pleased

--- Error_Search/app/javascript/components/Results/ResultsBody.jsx
+++ Error_Search/app/javascript/components/Results/ResultsBody.jsx
@@ -1,18 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const ResultsBody = ({tags, question_id, title, link, user}) => {
     const [showFavouritedStar ,setShowFavouritedStar] = useState(false)
+    const [response, setResponse] = useState("null")
+
+    useEffect(()=>{
+        setResponse(checkIfPostIsFavouriteToUser())
+    },[])
+
+
     const truncate = (str, max, suffix) => str.length < max ? str : `${str.substr(0, str.substr(0, max - suffix.length).lastIndexOf(' '))}${suffix}`;
     const ConvertAtSymbolInEmail = (userEmail) => {return userEmail.replace(/@/g, "%40")}
-
     const replaceJsonSyntax = (resultTitle) =>{
         return resultTitle.replace(/&#39;/g,'\'').replace(/&quot;/g,'"').replace(/&gt;/g,'>').replace(/&lt;/g,'<')
     }
 
-    const postFavourite = async (question_id, link) => {
+    const postFavourite = async (question_id) => {
         setShowFavouritedStar(!showFavouritedStar)
         const email = ConvertAtSymbolInEmail(user.email)
-        const url = `http://localhost:8080/backend/createfavourite/${email}/${question_id}`;
+        const url = `http://localhost:8080/backend/postfavourite/${email}/${question_id}`;
         const data = await fetch(url,
             {
                 method: 'POST',
@@ -23,6 +29,15 @@ const ResultsBody = ({tags, question_id, title, link, user}) => {
         const dataJson = await data.json();
         console.log(dataJson)
     }
+    const checkIfPostIsFavouriteToUser = async () => {
+        const email = ConvertAtSymbolInEmail(user.email)
+        const url = `http://localhost:8080/backend/findfavourite/${email}/${question_id}`;
+        const data = await fetch(url)
+        const dataJson = await data.json();
+        dataJson.favouritesID ? setShowFavouritedStar(!showFavouritedStar) : null
+        return dataJson.favouritesID ? dataJson : null
+    }
+
     return (
 
         <>
